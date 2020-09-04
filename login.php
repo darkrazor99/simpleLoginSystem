7098500801b24593239982219cfdad342edf9c2c
dark_razor99 <tarektheman99@gmail.com>
2020-09-04 16:02:33 +0200

    edited signup.php and login.php

--- login.php
+++ login.php
@@ -18,10 +18,11 @@
           }
          ?>
         <label>email</label>
-        <input type="text" name="email" value="<?php echo (isset($_POST['email']) ? $_POST['email'] : ''); ?>"  placeholder="email">
+        <input style="margin-bottom: 10px; width: 200px; margin-left: 34px;" type="text" name="email" value="<?php echo (isset($_POST['email']) ? $_POST['email'] : ''); ?>"  placeholder="email">
         <br>
         <label>password</label>
-        <input type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>" placeholder="password">
+        <input style="margin-bottom: 10px; width: 200px; margin-left: 10px;" type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>" placeholder="password">
+        <br>
         <button type="submit" name="login-submit">Login</button>
       </form>
       <a href="signup.php">Signup</a>
--- signup.php
+++ signup.php
@@ -17,19 +17,19 @@
         }
        ?>
       <label>name</label>
-      <input type="text" name="name" value="<?php echo (isset($_POST['name']) ? $_POST['name'] : ''); ?>" placeholder="name">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 60px;" type="text" name="name" value="<?php echo (isset($_POST['name']) ? $_POST['name'] : ''); ?>" placeholder="name">
       <br>
       <label>email</label>
-      <input type="text" name="email" value="<?php echo (isset($_POST['email']) ? $_POST['email'] : ''); ?>" placeholder="email">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 59px;" type="text" name="email" value="<?php echo (isset($_POST['email']) ? $_POST['email'] : ''); ?>" placeholder="email">
       <br>
       <label>password</label>
-      <input type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>" placeholder="password">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 35px;" type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>" placeholder="password">
       <br>
       <label>phone number</label>
-      <input type="text" name="phonenum" value="<?php echo (isset($_POST['phonenum']) ? $_POST['phonenum'] : ''); ?>" placeholder="phone number">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 3px;" type="text" name="phonenum" value="<?php echo (isset($_POST['phonenum']) ? $_POST['phonenum'] : ''); ?>" placeholder="phone number">
       <br>
       <label>address</label>
-      <input type="text" name="address" value="<?php echo (isset($_POST['address']) ? $_POST['address'] : ''); ?>" placeholder="address">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 47px;" type="text" name="address" value="<?php echo (isset($_POST['address']) ? $_POST['address'] : ''); ?>" placeholder="address">
       <br>
       <button type="submit" name="signup-submit">Signup</button>
     </form>
